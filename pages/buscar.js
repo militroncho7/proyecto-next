2aba8def6f979d8bd3fe86677dd86e1783807c6a
militroncho7 <vm.moralesruiz@gmail.com>
2021-09-07 14:03:18 +0200

    start project

--- pages/buscar.js
+++ pages/buscar.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Layout from '../components/layout/Layout';
 
-const Home = () => {
+const Buscar = () => {
   return (
     <div>
       <Layout>
-        <h1>Inicio</h1>
+        <h1>Buscar</h1>
       </Layout>
     </div>
   );
 }
  
-export default Home;
+export default Buscar;
--- pages/nuevo-producto.js
+++ pages/nuevo-producto.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Layout from '../components/layout/Layout';
 
-const Home = () => {
+const NuevoProductos = () => {
   return (
     <div>
       <Layout>
-        <h1>Inicio</h1>
+        <h1>Nuevo producto</h1>
       </Layout>
     </div>
   );
 }
  
-export default Home;
+export default NuevoProductos;
