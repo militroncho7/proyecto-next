81b53f3423c30c4eae6c7c59c5eb5645982417dd
militroncho7 <vm.moralesruiz@gmail.com>
2021-09-07 14:15:56 +0200

    formularios

--- pages/crear-cuenta.js
+++ pages/crear-cuenta.js
@@ -1,11 +1,52 @@
 import React from 'react';
 import Layout from '../components/layout/Layout';
+import { Formulario, Campo, InputSubmit } from '../components/UI/Formulario';
 
 const CrearCuenta = () => {
   return (
     <div>
       <Layout>
-        <h1>Crear Cuenta</h1>
+        <>
+          <h1>Crear Cuenta</h1>
+
+          <Formulario>
+            <Campo>
+              <label htmlFor="nombre">Nombre</label>
+              <input 
+                type="text"
+                id="nombre"
+                placeholder="Tu nombre"
+                name="nombre"
+              />
+            </Campo>
+
+            <Campo>
+              <label htmlFor="email">Email</label>
+              <input 
+                type="email"
+                id="email"
+                placeholder="Tu email"
+                name="email"
+              />
+            </Campo>
+
+            <Campo>
+              <label htmlFor="password">Password</label>
+              <input 
+                type="password"
+                id="password"
+                placeholder="Tu password"
+                name="password"
+              />
+            </Campo>
+
+            <InputSubmit
+              type="submit"
+              value="Crear Cuenta"
+            />
+          </Formulario>
+
+        </>
       </Layout>
     </div>
   );
